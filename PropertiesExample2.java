321fff0339c0bfdb84f47430a347cc80cc96a7b6
Immortvll <164056799+Immortvll@users.noreply.github.com>
2024-03-19 19:53:36 +0300

    Add files via upload

---
+++ PropertiesExample2.java
@@ -0,0 +1,12 @@
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Random;
+
+public class PropertiesExample2 {
+    public static void main(String[] args) {
+        String name = System.getProperty("name");
+        int random = 3012340;
+        String encoded = Base64.getEncoder().encodeToString(("" + random).getBytes(StandardCharsets.UTF_8));
+        System.out.println(name + " " + encoded);
+    }
+}
